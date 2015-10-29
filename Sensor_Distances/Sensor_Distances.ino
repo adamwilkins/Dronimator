f65a0a4b0f35f607fd05484848320f71a8e2a7a4
Stanley Wang <jeffywang1994@gmail.com>
2015-10-29 17:55:04 -0400

    Revert "Arduino Pitch And Roll"
    This reverts commit 53b87bae992ecd20c18784e47ec705dbb993f17b.

--- Sensor_Distances/Sensor_Distances.ino
+++ Sensor_Distances/Sensor_Distances.ino
@@ -1,42 +1,22 @@
 /* 
- * modified 10/27/2015
- * by Stanley Wang
- *  
  * created 10/17/2015
  * by Stanley Wang
  * 
  * Team Drominator
- * 
- * Get sonar sensor distances
- * Using HC-SR04 sonar sensors
  */
 
 /*
- * Each direction has its own trigger and echo pin
- * Pin sharing for trigger is allowed but not for echo
- * We are sharing trigger pins so echoes do not interfere
- * Each trigger has a 15 degree effectual angle
+ * All triggers should be on the same pin
+ * Each echo has it's own pin
  */
-const int NORTH_ECHO_PIN = 1;
-const int NORTH_TRIGGER_PIN = 8;
-
-const int EAST_ECHO_PIN = 2;
-const int EAST_TRIGGER_PIN = 8;
-
-const int WEST_ECHO_PIN = 3;
-const int WEST_TRIGGER_PIN = 8;
-
-const int NORTH_EAST_ECHO_PIN = 4;
-const int NORTH_EAST_TRIGGER_PIN = 9;
-
-const int NORTH_WEST_ECHO_PIN = 5;
-const int NORTH_WEST_TRIGGER_PIN = 9;
-
-const int UP_ECHO_PIN = 6;
-const int UP_TRIGGER_PIN = 9;
-
-const int DOWN_ECHO_PIN = 7;
-const int DOWN_TRIGGER_PIN = 9;
+const int echoTrigger = 1;
+const int echoNorth = 2;
+const int echoNorthEast = 3;
+const int echoNorthWest = 4;
+const int echoEast = 5;
+const int echoWest = 6;
+const int echoUp = 7;
+const int echoDown = 8;
 
 /*
  * Distance for each echo
@@ -78,30 +58,14 @@ void loop() {
  * sets up the pins for the sonar sensors
  */
 void setupEcho() {
-  pinMode(NORTH_ECHO_PIN, INPUT);
-  pinMode(NORTH_EAST_ECHO_PIN, INPUT);
-  pinMode(NORTH_WEST_ECHO_PIN, INPUT);
-  pinMode(EAST_ECHO_PIN, INPUT);
-  pinMode(WEST_ECHO_PIN, INPUT);
-  pinMode(UP_ECHO_PIN, INPUT);
-  pinMode(DOWN_ECHO_PIN, INPUT);
-  
-  pinMode(NORTH_TRIGGER_PIN, OUTPUT);
-  pinMode(NORTH_EAST_TRIGGER_PIN, OUTPUT);
-  pinMode(NORTH_WEST_TRIGGER_PIN, OUTPUT);
-  pinMode(EAST_TRIGGER_PIN, OUTPUT);
-  pinMode(WEST_TRIGGER_PIN, OUTPUT);
-  pinMode(UP_TRIGGER_PIN, OUTPUT);
-  pinMode(DOWN_TRIGGER_PIN, OUTPUT);
-
-  //make sure trigger pins are off first
-  digitalWrite(NORTH_TRIGGER_PIN, LOW);
-  digitalWrite(NORTH_EAST_TRIGGER_PIN, LOW);
-  digitalWrite(NORTH_WEST_TRIGGER_PIN, LOW);
-  digitalWrite(EAST_TRIGGER_PIN, LOW);
-  digitalWrite(WEST_TRIGGER_PIN, LOW);
-  digitalWrite(UP_TRIGGER_PIN, LOW);
-  digitalWrite(DOWN_TRIGGER_PIN, LOW);
+  pinMode(echoTrigger, OUTPUT);
+  pinMode(echoNorth, INPUT);
+  pinMode(echoNorthEast, INPUT);
+  pinMode(echoNorthWest, INPUT);
+  pinMode(echoEast, INPUT);
+  pinMode(echoWest, INPUT);
+  pinMode(echoUp, INPUT);
+  pinMode(echoDown, INPUT);
   
   Serial.println("Sensor setup complete");
 }
@@ -110,34 +74,57 @@ void setupEcho() {
  * Gets the distance of all 7 sonars
  * 0 = N, 1 = NE, 2 = NW, 3 = E, 4 = W, 5 = Up, 6 = Down
  * delay of 40 milliseconds to make sure there are no lingering triggers
- * Worst case senario delay of up to roughly half a second (280ms)
- * Worst case only happens if there is nothing
+ * Worst case senario delay of up to roughly half a second (560ms)
  */
 void getDistance(long *distance) {
   //do north/east/west to reduce interfering echos
-  distance[0] = getDistance(NORTH_TRIGGER_PIN, NORTH_ECHO_PIN);
-  distance[3] = getDistance(EAST_TRIGGER_PIN, EAST_ECHO_PIN);
-  distance[4] = getDistance(WEST_TRIGGER_PIN, WEST_ECHO_PIN);
-  
-  //up and down should not interfere with any other
-  distance[5] = getDistance(UP_TRIGGER_PIN, UP_ECHO_PIN);
-  distance[6] = getDistance(DOWN_TRIGGER_PIN, DOWN_ECHO_PIN);
+  distance[0] = getDistance(echoNorth);
+  delay(40);
+  distance[3] = getDistance(echoEast);
+  delay(40);
+  distance[4] = getDistance(echoWest);
+  delay(40);
   
   //do ne/nw next to reduce interfering echos
-  distance[1] = getDistance(NORTH_EAST_TRIGGER_PIN, NORTH_EAST_ECHO_PIN);
-  distance[2] = getDistance(NORTH_WEST_TRIGGER_PIN, NORTH_WEST_ECHO_PIN);
+  distance[1] = getDistance(echoNorthEast);
+  delay(40);
+  distance[2] = getDistance(echoNorthWest);
+  delay(40);
+  
+  //up and down should not interfere with each other
+  distance[5] = getDistance(echoUp);
+  delay(40);
+  distance[6] = getDistance(echoDown);
+  delay(40);
   
   Serial.println("All distances calculated");
 }
 
 /*
- * Gets a single distance back (in inches) from a selected trigger/echo pin
+ * Gets a single distance back (in inches) from a selected echo pin
  */
-long getDistance(int triggerPin, int echoPin) {
+long getDistance(int echoPin) {
   //HC-SR04 needs 10 micro seconds of trigger time
-  digitalWrite(triggerPin, HIGH);
+  digitalWrite(echoTrigger, HIGH);
   delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
+  digitalWrite(echoTrigger, LOW);
+
+  if (echoPin == echoNorth)
+    Serial.println("Calculating North");
+  else if (echoPin == echoNorthEast)
+    Serial.println("Calculating NorthEast");
+  else if (echoPin == echoNorthWest)
+    Serial.println("Calculating NorthWest");
+  else if (echoPin == echoEast)
+    Serial.println("Calculating East");
+  else if (echoPin == echoWest)
+    Serial.println("Calculating West");
+  else if (echoPin == echoUp)
+    Serial.println("Calculating Up");
+  else if (echoPin == echoDown)
+    Serial.println("Calculating Down");
+  else
+    Serial.println("Calculating ERROR");
 
   //from the HC-SR04 specs, pulse width(μs)/148 = distance(inches)
   //40 milliseconds timeout, specs say 38ms or no signal
